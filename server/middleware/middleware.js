3cd939bc72e71ade93d6c2d113b7152799e4949f
jakblak <schroe9@gmail.com>
2018-08-21 13:36:27 -0700

    new packages

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
+const passport = require('passport');
 
 // setup global middleware here
 module.exports = (app) => {
@@ -10,4 +11,7 @@ module.exports = (app) => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cookieParser());
+  app.use(require('express-session')({ secret: 'seZ*986', resave: true, saveUninitialized: true }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 };
--- server/models/user.js
+++ server/models/user.js
@@ -22,9 +22,10 @@ module.exports = (sequelize, DataTypes) => {
     },
     role: {
       type: DataTypes.ENUM,
-      values: ['user', 'admin', 'disabled']
-
-    }
+      values: ['user', 'admin', 'disabled'],
+      defaultValue: 'user'
+    },
+    avatar: DataTypes.STRING
   });
 
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
